00e96a33c225a8792b67dcf913a9b23d91b5cddd
MohamedReda25 <redam9618@gmail.com>
2021-12-22 23:49:19 +0200

    Comp_Decomp

--- XML_Editor/Compressing.cs
+++ XML_Editor/Compressing.cs
@@ -10,8 +10,164 @@ namespace XML_Editor
      * 
      * 
      */
-    internal class Compressing
+    internal class Node
     {
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
 
+        public List<bool> Traverse(char symbol, List<bool> data)
+        {
+            // Leaf
+            if (Right == null && Left == null)
+            {
+                if (symbol.Equals(this.Symbol))
+                {
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+
+                if (Left != null)
+                {
+                    List<bool> leftPath = new List<bool>();
+                    leftPath.AddRange(data);
+                    leftPath.Add(false);
+
+                    left = Left.Traverse(symbol, leftPath);
+                }
+
+                if (Right != null)
+                {
+                    List<bool> rightPath = new List<bool>();
+                    rightPath.AddRange(data);
+                    rightPath.Add(true);
+                    right = Right.Traverse(symbol, rightPath);
+                }
+
+                if (left != null)
+                {
+                    return left;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
     }
-}
+
+    internal class HuffmanTree
+    {
+        private List<Node> nodes = new List<Node>();
+        public Node Root { get; set; }
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public void Build(string source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+
+                if (!Frequencies.ContainsKey(source[i])) // if read the char the fisrt time, add it to the dictionary
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++; /* Advace the frequncy of the char*/
+            }
+
+            foreach (KeyValuePair<char, int> element in Frequencies)
+            {
+                Node n = new Node() { Symbol = element.Key, Frequency = element.Value };
+                nodes.Add(n); // Create nodes that contain the symbol and its frequency
+            }
+
+            while (nodes.Count > 1)
+            {
+                List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+
+                if (orderedNodes.Count >= 2)
+                {
+                    // Take first two items
+                    List<Node> pair = orderedNodes.Take(2).ToList<Node>();
+
+                    // Create a parent node by combining the frequencies
+                    Node parent = new Node()
+                    {
+                        Symbol = '*',
+                        Frequency = pair[0].Frequency + pair[1].Frequency,
+                        Left = pair[0],
+                        Right = pair[1]
+                    };
+
+                    nodes.Remove(pair[0]);
+                    nodes.Remove(pair[1]);
+                    nodes.Add(parent);
+                }
+
+                this.Root = nodes[0];
+
+            }
+
+        }
+
+        public BitArray Encode(string source)
+        {
+            List<bool> totalEncoded = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                totalEncoded.AddRange(encodedSymbol); // Add symbol encoded to total encoded code
+            }
+
+            BitArray bits = new BitArray(totalEncoded.ToArray());
+
+            return bits;
+        }
+
+        public string Decode(BitArray bits)
+        {
+            Node current = this.Root;
+            string decoded = "";
+
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                    }
+                }
+
+                if (IsLeaf(current))
+                {
+                    decoded += current.Symbol;
+                    current = this.Root;
+                }
+            }
+
+            return decoded;
+        }
+
+        public bool IsLeaf(Node node)
+        {
+            return (node.Left == null && node.Right == null);
+        }
+    }
